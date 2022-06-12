9e108209fccdca083a77adc7164b7773106d70e6
Allen Burton <adburton@student.fullsail.edu>
2022-06-12 10:20:32 -0700

    Add Button Update

--- calcApp/calcMain.cpp
+++ calcApp/calcMain.cpp
@@ -4,10 +4,10 @@
 
 calcMain::calcMain() : wxFrame(nullptr, wxID_ANY, "Allen's Calculator App", wxPoint(600, 300), wxSize(330, 500))
 {
-	m_btn1 = new wxButton(this, wxID_ANY, "Clear", wxPoint(10, 70), wxSize(50, 50));
-	m_btn2 = new wxButton(this, wxID_ANY, "%", wxPoint(90, 70), wxSize(50, 50));
-	m_btn3 = new wxButton(this, wxID_ANY, "(", wxPoint(170, 70), wxSize(50, 50));
-	m_btn4 = new wxButton(this, wxID_ANY, ")", wxPoint(250, 70), wxSize(50, 50));
+	m_btn1 = new wxButton(this, wxID_ANY, "C", wxPoint(10, 70), wxSize(50, 50));
+	m_btn2 = new wxButton(this, wxID_ANY, "MOD", wxPoint(90, 70), wxSize(50, 50));
+	m_btn3 = new wxButton(this, wxID_ANY, "BIN", wxPoint(170, 70), wxSize(50, 50));
+	m_btn4 = new wxButton(this, wxID_ANY, "HEX", wxPoint(250, 70), wxSize(50, 50));
 	m_btn5 = new wxButton(this, wxID_ANY, "7", wxPoint(10, 150), wxSize(50, 50));
 	m_btn6 = new wxButton(this, wxID_ANY, "8", wxPoint(90, 150), wxSize(50, 50));
 	m_btn7 = new wxButton(this, wxID_ANY, "9", wxPoint(170, 150), wxSize(50, 50));
@@ -15,7 +15,7 @@ calcMain::calcMain() : wxFrame(nullptr, wxID_ANY, "Allen's Calculator App", wxPo
 	m_btn9 = new wxButton(this, wxID_ANY, "4", wxPoint(10, 230), wxSize(50, 50));
 	m_btn10 = new wxButton(this, wxID_ANY, "5", wxPoint(90, 230), wxSize(50, 50));
 	m_btn11 = new wxButton(this, wxID_ANY, "6", wxPoint(170, 230), wxSize(50, 50));
-	m_btn12 = new wxButton(this, wxID_ANY, "x", wxPoint(250, 230), wxSize(50, 50));
+	m_btn12 = new wxButton(this, wxID_ANY, "*", wxPoint(250, 230), wxSize(50, 50));
 	m_btn13 = new wxButton(this, wxID_ANY, "1", wxPoint(10, 310), wxSize(50, 50));
 	m_btn14 = new wxButton(this, wxID_ANY, "2", wxPoint(90, 310), wxSize(50, 50));
 	m_btn15 = new wxButton(this, wxID_ANY, "3", wxPoint(170, 310), wxSize(50, 50));
